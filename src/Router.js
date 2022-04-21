b6718ae949fc9f287b294f95136b320724578e45
giordano-toscano <giordanotoscano@outlook.com>
2022-04-21 15:02:29 -0300

    create layout utilizing templates from Mantine

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,19 @@
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import Schedules from "./pages/Schedules";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/registration" element={<Registration />} />
-                <Route path="/schedules" element={<Schedules />} />
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/registration" element={<Registration />} />
+                    <Route path="/schedules" element={<Schedules />} />
+                </Routes>
+            </Layout>
         </BrowserRouter>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Router from "./Router";
+import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
 
-ReactDOM.render(<Router />, document.getElementById("root"));
+ReactDOM.render(
+    <MantineProvider>
+        <ColorSchemeProvider>
+            <Router />
+        </ColorSchemeProvider>
+    </MantineProvider>,
+    document.getElementById("root")
+);
